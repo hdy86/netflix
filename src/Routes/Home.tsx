c34437ada4444825bc9bff0e0450a5fa172f96ac
hdy86 <dyhong86@naver.com>
2022-10-14 16:45:07 +0900

    Create Slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utilities";
+import { isVisible } from "@testing-library/user-event/dist/utils";
 
 const Wrapper = styled.div`
   background: black;
@@ -13,6 +16,7 @@ const Loader = styled.div`
   height: 100vh;
   text-align: center;
 `;
+
 const Banner = styled.div<{ bgPhoto: string }>`
   display: flex;
   justify-content: center;
@@ -36,11 +40,94 @@ const Overview = styled.p`
   font-size: 30px;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  position: absolute;
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  width: 100%;
+`;
+const Box = styled(motion.div)`
+  font-size: 66px;
+
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+const Img = styled.div<{ bgPhoto: string }>`
+  width: 100%;
+  height: 200px;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center;
+`;
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
+const rowVariants = {
+  hidden: { x: window.outerWidth + 5 },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth - 5 },
+};
+const boxVariants = {
+  normal: { scale: 1 },
+  hover: {
+    y: -50,
+    scale: 1.3,
+    transition: {
+      type: "tween",
+      delay: 0.5,
+      duaration: 0.3,
+    },
+  },
+};
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      type: "tween",
+      delay: 0.5,
+      duaration: 0.3,
+    },
+  },
+};
+
+const offset = 6; // 한 페이지에 보여지는 영화 갯수
+const NEXFLIX_LOGO_URL =
+  "https://assets.brand.microsites.netflix.io/assets/2800a67c-4252-11ec-a9ce-066b49664af6_cm_800w.jpg?v=4";
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
 
   return (
     <Wrapper>
@@ -48,10 +135,49 @@ function Home() {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      variants={boxVariants}
+                      whileHover="hover"
+                      initial="normal"
+                      transition={{ type: "tween" }}
+                    >
+                      <Img
+                        bgPhoto={
+                          movie.backdrop_path
+                            ? makeImagePath(movie.backdrop_path, "w500")
+                            : NEXFLIX_LOGO_URL
+                        }
+                      />
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
