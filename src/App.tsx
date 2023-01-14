5687d827f3b354635ca4a65ce045ad9c2cc3aebb
hdy86 <dyhong86@naver.com>
2023-01-14 23:34:06 +0900

    Router 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Components/Header";
 import Movie from "./Routes/Movie";
